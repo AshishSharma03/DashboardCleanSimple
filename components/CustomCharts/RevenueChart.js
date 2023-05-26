80dc7a8feb47c23dae2ddb7db7ef4d3c2b43a424
AshishSharma03 <30octaksharma@gmail.com>
2023-05-26 21:40:06 +0530

    new commt

--- components/CustomCharts/RevenueChart.js
+++ components/CustomCharts/RevenueChart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
-// const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
-import ReactApexChart from 'react-apexcharts';
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+// import ReactApexChart from 'react-apexcharts';
 
 const RevenueChart = ({Data}) => {
   const [isClient, setIsClient] = useState(false);
